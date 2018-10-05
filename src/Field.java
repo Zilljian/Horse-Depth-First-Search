869e39b71c2ee624876f08fe235e7093b1853065
Ilya Balakin <victorieuse48@gmail.com>
2018-10-06 01:54:30 +0300

    Initial commit

---
+++ src/Field.java
@@ -0,0 +1,177 @@
+public class Field {
+
+    private String fieldToString = "Not initialized";
+    private int field[][];
+    private int OX, OY;
+    private int x0, y0, x, y;
+    private int minPath;
+    private int tempCounter = 0;
+
+    Field(int N, int M, int x0, int y0, int x, int y) {
+        OX = N;
+        OY = M;
+        minPath = OX > OY ? OX : OY;
+        this.x0 = x0 - 1;
+        this.y0 = y0 - 1;
+        this.x = x - 1;
+        this.y = y - 1;
+
+        if (isProper()) {
+            initialize();
+            findSearch(this.x0, this.y0);
+            fieldToString = Integer.toString(minPath);
+        }
+    }
+
+    private void initialize() {
+        field = new int[OY][OX];
+
+        for (int i = 0; i < OY; i++){
+            for (int j = 0; j < OX; j++){
+                field[i][j] = minPath;
+            }
+        }
+
+        field[x0][y0] = -1;
+        fieldToString = "Initialized";
+    }
+
+    private boolean isProper() {
+
+        if ((x0 >= OX) || (y0 >= OY) || (x0 < 0) || (y0 < 0)) {
+            fieldToString = "Failure: initial position of figure must be in range of field!";
+            return false;
+        }
+
+        if (((OY < 3) && (OX < 3)) || (OY < 2) || (OX < 2)){
+            fieldToString = "Failure: input data don't satisfy the conditions!";
+            return  false;
+        }
+
+        if ((OY ==3) && (OX == 3) && ((x0 != x) || (y0 != y)) && (x == y) && (x == 2)){
+            fieldToString = "Failure: input data don't satisfy the conditions. Purposely set inaccessible cell!";
+            return false;
+        }
+
+        return true;
+    }
+
+    private void findSearch(int x, int y){
+        if (minPath >= tempCounter + 1) {
+
+            if ((x + 2) >= 0 && (x + 2) < OX && (y + 1) >= 0 && (y + 1) < OY && field[y + 1][x + 2] > tempCounter + 1) {
+                tempCounter++;
+                field[y + 1][x + 2] = tempCounter;
+
+                if (isDestination(x + 2, y + 1)) {
+                    minPath = tempCounter;
+                    tempCounter--;
+                } else {
+                    findSearch(x + 2, y + 1);
+                }
+
+                tempCounter--;
+            }
+
+            if ((x + 2) >= 0 && (x + 2) < OX && (y - 1) >= 0 && (y - 1) < OY && field[y - 1][x + 2] > tempCounter + 1) {
+                tempCounter++;
+                field[y - 1][x + 2] = tempCounter;
+
+                if (isDestination(x + 2, y - 1)) {
+                    minPath = tempCounter;
+                } else {
+                    findSearch(x + 2, y - 1);
+                }
+
+                tempCounter--;
+            }
+
+            if ((x + 1) >= 0 && (x + 1) < OX && (y + 2) >= 0 && (y + 2) < OY && field[y + 2][x + 1] > tempCounter + 1) {
+                tempCounter++;
+                field[y + 2][x + 1] = tempCounter;
+
+                if (isDestination(x + 1, y + 2)) {
+                    minPath = tempCounter;
+                } else {
+                    findSearch(x + 1, y + 2);
+                }
+
+                tempCounter--;
+            }
+
+            if ((x - 1) >= 0 && (x - 1) < OX && (y + 2) > 0 && (y + 2) < OY && field[y + 2][x - 1] > tempCounter + 1) {
+                tempCounter++;
+                field[y + 2][x - 1] = tempCounter;
+
+                if (isDestination(x - 1 , y + 2)) {
+                    minPath = tempCounter;
+                } else {
+                    findSearch(x - 1, y + 2);
+                }
+
+                tempCounter--;
+            }
+
+            if ((x - 2) >= 0 && (x - 2) < OX && (y + 1) > 0 && (y + 1) < OY && field[y + 1][x - 2] > tempCounter + 1) {
+                tempCounter++;
+                field[y + 1][x - 2] = tempCounter;
+
+                if (isDestination(x - 2, y + 1)) {
+                    minPath = tempCounter;
+                } else {
+                    findSearch(x - 2, y + 1);
+                }
+
+                tempCounter--;
+            }
+
+            if ((x - 1) >= 0 && (x - 1) < OX && (y - 2) >= 0 && (y - 2) < OY && field[y - 2][x - 1] > tempCounter + 1) {
+                tempCounter++;
+                field[y - 2][x - 1] = tempCounter;
+
+                if (isDestination(x - 1, y - 2)) {
+                    minPath = tempCounter;
+                } else {
+                    findSearch(x - 1, y - 2);
+                }
+
+                tempCounter--;
+            }
+
+            if ((x + 1) >= 0 && (x + 1) < OX && (y - 2) >= 0 && (y - 2) < OY && field[y - 2][x + 1] > tempCounter + 1) {
+                tempCounter++;
+                field[y - 2][x + 1] = tempCounter;
+
+                if (isDestination(x + 1, y - 2)) {
+                    minPath = tempCounter;
+                } else {
+                    findSearch(x + 1, y - 2);
+                }
+
+                tempCounter--;
+            }
+
+            if ((x - 2) >= 0 && (x - 2) < OX && (y - 1) >= 0 && (y - 1) < OY && field[y - 1][x - 2] > tempCounter + 1) {
+                tempCounter++;
+                field[y - 1][x - 2] = tempCounter;
+
+                if (isDestination(x - 2, y - 1)) {
+                    minPath = tempCounter;
+                } else {
+                    findSearch(x - 2, y - 1);
+                }
+
+                tempCounter--;
+            }
+        }
+    }
+
+    private boolean isDestination(int x1, int y1){
+        return (x1 == x) && (y1 == y);
+    }
+
+    @Override
+    public String toString () {
+        return fieldToString;
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.io.*;
+import java.math.*;
+
+public class main {
+    public static void main (String[] args) throws IOException {
+        int x, x1, y, y1, OY, OX;
+
+        try(FileReader file = new FileReader("input.txt")) {
+            BufferedReader buffer = new BufferedReader(file);
+            String s = buffer.readLine();
+
+            int[] bufferInt = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).toArray();
+            OX = bufferInt[0];
+            OY = bufferInt[1];
+            x = bufferInt[2];
+            y = bufferInt[3];
+            x1 = bufferInt[4];
+            y1 = bufferInt[5];
+
+        }catch(IOException e){
+
+            OY = OX = 8;
+
+            System.out.print("Exception while trying to open file! Initial field " +
+                    "size has been set at 8x8");
+            System.out.print("Please, set initial position of arriving and destination cells>>");
+            BufferedReader initialization = new BufferedReader(new InputStreamReader(System.in));
+
+            x = Integer.parseInt(initialization.readLine());
+            y = Integer.parseInt(initialization.readLine());
+            x1 = Integer.parseInt(initialization.readLine());
+            y1 = Integer.parseInt(initialization.readLine());
+        }
+
+        BigDecimal timeStart = new BigDecimal(System.nanoTime());
+
+        Field field = new Field(OX, OY, x, y, x1, y1);
+
+        BigDecimal nanos = new BigDecimal(System.nanoTime()).subtract(timeStart);
+        BigDecimal seconds = nanos.divide(new BigDecimal(1e+9));
+        System.out.println(seconds);
+
+        System.out.print(field.toString());
+    }
+}
